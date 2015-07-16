af51d14c66a75989bad5f06e63c3d0055d726151
Blaine Fricks <blaine.fricks@live.com>
2015-07-16 08:31:10 -0700

    looking at how to grab itemName and itemDescription and inserting them into the html.

--- app/scripts/getting-started.js
+++ app/scripts/getting-started.js
@@ -1,13 +1,17 @@
 var apiKey = "YOUR_API_KEY_HERE";
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.open(
+  "GET",
+  "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/",
+  true);
 xhr.setRequestHeader("X-API-Key", apiKey);
 
 xhr.onreadystatechange = function(){
   if (this.readyState === 4 && this.status === 200) {
     var json = JSON.parse(this.responseText);
-    console.log(json.Response.data.inventoryItem.itemName);
+    document.getElementById("itemName").innerHTML = json.Response.data.inventoryItem.itemName;
+    document.getElementById("itemDescription").innerHTML = json.Response.data.inventoryItem.itemDescription;
   };
 }
 
