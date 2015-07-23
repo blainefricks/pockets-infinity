8b25aecbecbce84e3cde0d0e97ce48d99ecc2986
Blaine Fricks <blaine.fricks@live.com>
2015-07-23 08:52:38 -0700

    wrapped BungieService factory in self calling function.

--- app/scripts/services/dubBungieServices.factory.js
+++ app/scripts/services/dubBungieServices.factory.js
@@ -1,23 +1,29 @@
-dubApp.factory('dubBungieServices', function() {
+(function(){
 
-  function getCurrentUser() {
-    return {
-      method: 'GET',
-      url: 'https://www.bungie.net/platform/user/getbungienetuser/',
-      headers: {
-        'X-API-Key': apiKey,
+  dubApp.factory('dubBungieServices', BungieService);
+
+  BungieService.$inject = ['$q'];
+
+   function BungieService($q) {
+
+    function getCurrentUser() {
+      return {
+        method: 'GET',
+        url: 'https://www.bungie.net/platform/user/getbungienetuser/',
+        headers: {
+          'X-API-Key': apiKey,
+        }
+      };
+    };
+
+    function getGuardianInventoryRequest(platform, membershipId, character) {
+      return {
+        method: 'GET',
+        url: 'https://bungie.net/Destiny/' + platform + '/Account/' + membershipId + '/Character/' + character + '/Inventory/',
+        headers: {
+          'X-API-Key': apiKey,
+        }
       }
     };
-  };
-
-  function getGuardianInventoryRequest(platform, membershipId, character) {
-    return {
-      method: 'GET',
-      url: 'https://bungie.net/Destiny/' + platform + '/Account/' + membershipId + '/Character/' + character + '/Inventory/',
-      headers: {
-        'X-API-Key': apiKey,
-      }
-    }
-  };
-
-});
+  }
+})();
