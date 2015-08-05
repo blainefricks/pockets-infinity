daf61e43be4df931b09d5727776aa7fe4cdcdd4d
Blaine Fricks <blaine.fricks@live.com>
2015-08-05 09:32:04 -0700

    #2: getBungieCookies now returns bungled value.

--- app/scripts/services/dubBungieService.factory.js
+++ app/scripts/services/dubBungieService.factory.js
@@ -8,54 +8,61 @@
 
     var bungledValue = null;
 
-    function getBungieCookies() {
-      chrome.cookies.getAll({
+
+    /*********************************************************/
+
+
+    function getBungieCookies(callback) {
+      return chrome.cookies.getAll({
         'domain' : '.bungie.net',
-        // 'name' : 'bungled'
-      }, function (cookies) {
+        'name' : 'bungled'
+      }, getAllCallback.bind(this, callback));
+
+      function getAllCallback(callback, cookies) {
         if (cookies.length > 0) {
-          // Cookies found.
-          console.log(cookies);
+          callback(cookies[0].value);
         } else{
           alert("Error: No cookies found.");
         };
-      });
+      };
     };
 
-    function getBungledValue() {
-      // Get the 'value' field of the cookie named 'bungled'
-      bungledValue = getBungieCookies();
+    getBungieCookies(getCurrentUserRequest);
 
-      return bungledValue;
-    }
-    console.log("Bungled Value: " + getBungledValue());
 
-    function getCurrentUserRequest() {
+    /*********************************************************/
+
+
+    function getCurrentUserRequest(token) {
+      console.log(token); // dev
       return {
         method: 'GET',
         url: 'https://www.bungie.net/platform/user/getbungienetuser/',
         headers: {
           'X-API-Key': apiKey,
+          'x-csrf': token
         }
       };
     }
 
-    function getGuardiansRequest() {
+    function getGuardiansRequest(token) {
       return {
         method: 'GET',
         url: '',
         headers: {
           'X-API-Key': apiKey,
+          'x-csrf': token
         }
       };
     }
 
-    function getGuardianInventoryRequest(platform, membershipId, character) {
+    function getGuardianInventoryRequest(token, platform, membershipId, character) {
       return {
         method: 'GET',
         url: 'https://bungie.net/Destiny/' + platform + '/Account/' + membershipId + '/Character/' + character + '/Inventory/',
         headers: {
           'X-API-Key': apiKey,
+          'x-csrf': token
         }
       };
     }
