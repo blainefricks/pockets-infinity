07948ee53d54d84d9a3c1cf8ecf515dda91016b4
Blaine Fricks <blaine.fricks@live.com>
2015-08-08 15:32:44 -0700

    #13: Refactored promise code and added console logs for debugging.

--- app/scripts/services/dubBungieService.factory.js
+++ app/scripts/services/dubBungieService.factory.js
@@ -3,30 +3,10 @@
   dubApp.factory('dubBungieService', BungieService);
 
    function BungieService() {
-
-
-    // function apiRequest (method, url, token, request) {
-    //   var xhr = new XMLHttpRequest();
-
-    //   xhr.open(method, url, true);
-    //   xhr.setRequestHeader("X-API-Key", apiKey, "X-CSRF", token);
-
-    //   xhr.onreadystatechange = function(){
-    //     if (this.readyState === 4 && this.status === 200) {
-    //       var json = JSON.parse(this.responseText);
-    //       if (json.ErrorCode > 1) {
-    //         alert("Error: " + json.ErrorStatus + "\n" + json.Message);
-    //         console.log(json); // dev: log error
-    //       } else {
-    //         console.log(json.request); // dev: log response
-    //       };
-    //     };
-    //   }
-
-    //   xhr.send();
-    // }
+    var currentUserPromise = null;
 
     function apiRequest(method, url, token) {
+      console.log("apiRequest(" + method + ", " + url + ", " + token + ")"); // dev
       return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
 
@@ -36,8 +16,7 @@
         xhr.onreadystatechange = function() {
           var json = JSON.parse(this.responseText);
           resolve(response);
-          reject(console.log("apiRequestPromise is rejected;"));
-          alert("test");
+          reject(console.log("apiRequest is rejected;"));
         }
       });
     }
@@ -46,7 +25,7 @@
     /*********************************************************/
 
 
-    function getBungieCookies(callback) {
+    function getBungieCookies() {
       return new Promise(function(resolve, reject) {
         chrome.cookies.getAll({
           'domain': '.bungie.net',
@@ -55,8 +34,8 @@
 
         function getAllCallback(cookies) {
           if (cookies.length > 0) {
+            console.log("cookies[0].value: " + cookies[0].value); // dev
             resolve(cookies[0].value);
-            console.log(cookies[0].value); // dev-note: Bungled Cookie Value
           } else{
             reject(alert("Error: No cookies found."));
           };
@@ -64,25 +43,32 @@
       });
     }
 
-    getBungieCookies(getCurrentUserRequest);
-
 
     /*********************************************************/
 
 
+    function getCurrentUser() {
+      currentUserPromise = currentUserPromise || getBungieCookies()
+        .then(getCurrentUserRequest);
+      console.log(currentUserPromise); // dev
+      return currentUserPromise;
+    }
+
+    getCurrentUser();
+
     function getCurrentUserRequest(token) {
       // Returns account information for the current user
-      alert("getCurrentUserRequest");
+      console.log("getCurrentUserRequest(" + token + ")"); // dev-note: Bungled Cookie Value
       apiRequest("GET", "https://www.bungie.net/Platform/User/GetBungieNetUser/", token)
         .then(function(json) {
-          console.log(json.Response.user.membershipId);
+          console.log(json.Response.user.membershipId); // dev: log the response for testing
         });
     }
 
     function getGuardiansRequest(token) {
-      apiRequest("GET", "",token, request)
+      apiRequest("GET", "",token)
         .then(function(json) {
-          console.log(json.Response);
+          console.log(json);
         });
     }
 
