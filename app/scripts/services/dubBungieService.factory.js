09d6af464d267acdc1891d2ad123194fbf82b488
Blaine Fricks <blaine.fricks@live.com>
2015-08-14 07:43:58 -0700

    #11: working on guardian inventory api requests

--- app/scripts/services/dubBungieService.factory.js
+++ app/scripts/services/dubBungieService.factory.js
@@ -4,6 +4,7 @@
 
    function BungieService() {
     var bungieNetUserPromise = null;
+    var guardianPromise = null;
 
     var factory = {
       getBungieNetUser : getBungieNetUser
@@ -48,7 +49,6 @@
 
         function getAllCallback(cookies) {
           if (cookies.length > 0) {
-            console.log("bungled cookie value:", cookies[0].value); // dev
             resolve(cookies[0].value);
           } else {
             reject(alert("Error: No cookies found."));
@@ -70,7 +70,9 @@
         .then(processBungieNetRequest)
         .catch(function(error) {
           console.log("Failed!", error);
-        });
+        })
+        .then(generateMembership)
+        .then(getGuardians);
 
       return bungieNetUserPromise;
     }
@@ -89,28 +91,100 @@
       console.log("processBungieNetRequest(response)", response); // dev
       if (response.ErrorCode > 1) {
         console.log(response.Message);
-      } else {
-        console.log(response.Response.user); // dev: log the response for testing
       };
 
       return response;
     }
 
-    function getGuardiansRequest(token) {
+    function generateMembership(response) {
+      var userData = response.Response.user;
+      var platformId = null;
+
+      if (userData.xboxDisplayName) {
+        platformId = 1;
+        console.log("Platform: Xbox"); // dev
+        handle = userData.xboxDisplayName;
+        console.log(handle); // dev
+      }
+
+      if (userData.psnDisplayName) {
+        platformId = 2;
+        console.log("Platform: PlayStation"); // dev
+        handle = userData.psnDisplayName;
+        console.log(handle); // dev
+      }
+      console.log("membershipId:",userData.membershipId);
+
       return {
-        method : "GET",
-        url : "",
+        handle : handle,
+        id : userData.membershipId,
+        platform : platformId
+      };
+    }
+
+
+    /*********************************************************/
+
+
+    function getGuardians(membership) {
+      console.log(membership); // dev
+
+      guardianPromise = getBungieCookies()
+        .then(getGuardiansRequest)
+        .then(apiRequest)
+        .then(processGuardiansRequest);
+
+      return guardianPromise;
+    }
+
+    function getGuardiansRequest(token, membership) {
+      console.log(membership); // dev
+      console.log("token : " + token + "\n" + "membership : " + membership); // dev
+      return {
+        method : 'GET',
+        url: 'https://www.bungie.net/Platform/User/GetBungieAccount/' + membership.id + '/' + membership.platform + '/',
         token : token
       }
     }
 
-    function getGuardianInventoryRequest(token, membershipType, membershipId, character) {
+    function processGuardiansRequest(response) {
+      console.log(response); // dev
+    }
+
+    function generateGuardians(response) {
+      var guardianData = response.Response.destinyAccounts.characters;
+
+      return {
+        id : guardianId
+      }
+    }
+
+
+    /*********************************************************/
+
+
+    function getInventories() {
+      var promise = getBungieCookies()
+      .then(getGuardianInventoryRequest)
+      .then(apiRequest)
+      .then(processGuardianInventory);
+
+      return promise;
+    }
+
+    function getGuardianInventoryRequest(token, membership, guardian) {
+      console.log("getGuardianInventoryRequest(token)", token); // dev
       // Returns the inventory for the supplied character.
       return {
         method : "GET",
-        url : 'https://bungie.net/Destiny/' + membershipType + '/Account/' + membershipId + '/Character/' + character.id + '/Inventory/',
+        url : 'https://bungie.net/Destiny/' + membership.type + '/Account/' + membership.id + '/Character/' + guardian.id + '/Inventory/',
         token : token
       }
+    }
+
+    function processGuardianInventory(response) {
+      console.log("processGuardianInventory(response)", response); // dev
+
     }
   }
 })();
